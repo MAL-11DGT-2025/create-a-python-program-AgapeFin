d7c9cf99727a998107c0886a77d92e9755a4759d
agape-fineanganofo <agape.fineanganofo@marcellin.school.nz>
2025-03-04 10:46:09 +1300

    Added Comments

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
-print("Hello there, User.")
+print("Hello there, User.") # Introduction
 print("My name is Agape!")
 
-User_Name = input("What is your name? ")
+User_Name = input("What is your name? ") # This asks the user for their name.
 print(f"Hello {User_Name}! It is nice to meet you!")
 
-User_Age = int(input("How old are you? "))
+User_Age = int(input("How old are you? ")) # This asks the user for their age.
 if User_Age < 18:
-    print(f"Wow, you are {User_Age}? You are quite young!")
+    print(f"Wow, you are {User_Age}? You are quite young!") # Here are the different outcomes for giving a different age range.
 elif User_Age < 49:
     print(f"You are {User_Age}? You are aging.")
 elif User_Age > 50:
