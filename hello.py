60fc50ed3161e087da1d4519b2b6dcba417b536a
agape-fineanganofo <agape.fineanganofo@marcellin.school.nz>
2025-03-04 10:43:01 +1300

    Added new inputs (Name and Age)

--- hello.py
+++ hello.py
@@ -1,2 +1,15 @@
 print("Hello there, User.")
-print("My favourite device to use is a laptop!")
+print("My name is Agape!")
+
+User_Name = input("What is your name? ")
+print(f"Hello {User_Name}! It is nice to meet you!")
+
+User_Age = int(input("How old are you? "))
+if User_Age < 18:
+    print(f"Wow, you are {User_Age}? You are quite young!")
+elif User_Age < 49:
+    print(f"You are {User_Age}? You are aging.")
+elif User_Age > 50:
+    print(f"You are way over your 50s? You are old.")
+
+
