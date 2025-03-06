29f68e229eb3c3b8a45094ee637396291eb84cd3
agape-fineanganofo <agape.fineanganofo@marcellin.school.nz>
2025-03-07 12:54:23 +1300

    Added new file, Worked with list functions.

---
+++ lists.py
@@ -0,0 +1,12 @@
+colours = ["red", "green", "blue", "purple", "brown"]
+# print(colours[1])
+
+# for i in colours:
+#    print(i)
+
+# print(colours[-3])
+
+# print(len(colours))
+# for i in colours:
+#    if i == "blue":
+#        print("There is blue.")
