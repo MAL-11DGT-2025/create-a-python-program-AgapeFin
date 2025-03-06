9d9d8fe157d9d02df07405376af0c3b8b6b35e35
agape-fineanganofo <agape.fineanganofo@marcellin.school.nz>
2025-03-06 14:43:45 +1300

    Added False and True Statements, Added Comments

--- hello.py
+++ hello.py
@@ -1,15 +1,25 @@
-print("Hello there, User.") # Introduction
-print("My name is Agape!")
+# print("Hello there, User.") # Introduction
+# print("My name is Agape!")
 
-User_Name = input("What is your name? ") # This asks the user for their name.
-print(f"Hello {User_Name}! It is nice to meet you!")
+# User_Name = input("What is your name? ") # This asks the user for their name.
+# print(f"Hello {User_Name}! It is nice to meet you!")
 
-User_Age = int(input("How old are you? ")) # This asks the user for their age.
-if User_Age >= 13 and User_Age <= 19:
-    print(f"Wow, you are {User_Age}? You are a Teenager!") # Here are the different outcomes for giving a different age range.
-elif User_Age < 49:
-    print(f"You are {User_Age}? You are aging.")
-elif User_Age > 50:
-    print("You are way over your 50s? You are old.")
+# User_Age = int(input("How old are you? ")) # This asks the user for their age.
+# if User_Age >= 13 and User_Age <= 19:
+#   print(f"Wow, you are {User_Age}? You are a Teenager!") # Here are the different outcomes for giving a different age range.
+# elif User_Age >= 13 or User_Age <= 19:
+#    print("You might be a teenager.")
 
+# FALSE AND TRUE STATEMENTS #
 
+# Umbrella = False
+# Sunny = True
+
+# if Umbrella and Sunny:
+#    print("You think it it's going to rain.")
+# elif not Umbrella and Sunny:
+#    print("You think you don't need an umbrella.")
+# elif Umbrella and not Sunny:
+#    print("It must be raining.")
+# elif not Umbrella and not Sunny:
+#    print("You must be drenched.")
