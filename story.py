c79ee16810eae9a7504b728fdc83f0e49ca52977
agape-fineanganofo <agape.fineanganofo@marcellin.school.nz>
2025-03-06 15:11:40 +1300

    Added a new file. Created a story function for User to Input.

---
+++ story.py
@@ -0,0 +1,14 @@
+def tell_a_story(age, name, car, food, whanau, game):
+    
+    
+    print(f"Hello {name}, I also love {game}. You are {age}? You are kind of old. You should have a job by now. You like {food}, that is fine. You're favourite car is {car}? That is amazing. You are in {whanau}? That is the worst house.")
+
+
+age = input("How old are you: ")
+name = input("What is your name: ")
+car = input("What is your favourite car: ")
+food = input("What is the worst food: ")
+whanau = input("What is the worst whanau/house: ")
+game = input("What is your favourite game: ")
+
+tell_a_story(age, name, car, food, whanau, game)
