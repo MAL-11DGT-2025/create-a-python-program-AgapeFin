8eaf12bee36d54e51ba83ebfabcc43b2422ea1ad
agape-fineanganofo <agape.fineanganofo@marcellin.school.nz>
2025-03-07 13:30:27 +1300

    Added new file, if and else statments.

---
+++ words.py
@@ -0,0 +1,20 @@
+# season = input("What is your favourite season: ")
+
+# season_list = ["summer", "winter", "autumn", "spring", "fall"]
+
+# if season.lower().upper().strip() in season_list:
+#    print("That is a valid season.")
+# else:
+#    print("That is not a valid season.")
+
+# print("A)Leviticus\nB)Genesis\nC)Numbers\nD)Exodus")
+# biblebooks = input ("What is the first book of the bible: ")
+
+# bible_correct = ["B", "b", "Genesis", "genesis"]
+# bible_incorrect = ["D", "d", "C", "c", "A", "a" "Exodus", "Numbers", "Leviticus"]
+# if biblebooks.lower().strip() in bible_correct:
+#    print("Genesis is the first book! You are correct.")
+# elif biblebooks.lower().strip() in bible_incorrect:
+#    print("Incorrect answer.")
+# else:
+#    print("Invalid answer.")
